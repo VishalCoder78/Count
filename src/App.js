421671ba954efc9da04ae2c31b68d24f58f1d41a
VishalCoder78 <vishalcoder78@gmail.com>
2023-05-03 01:16:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./App.css";
+import "./App.css";
 import { useEffect, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 
